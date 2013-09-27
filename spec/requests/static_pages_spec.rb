1a7f701f5326a02c4a810404b97e211ed784c18a
Heidar Bernhardsson <heidar@iseld.org>
2013-09-27 01:11:47 -0700

    Add full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title" do
       visit '/static_pages/home'
       expect(page).to have_title("#{base_title} | Home")
     end
